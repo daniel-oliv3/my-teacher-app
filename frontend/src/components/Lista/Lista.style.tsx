57a5e46fa91ed12cab3cf710c4ee9a305d2d2122
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-10-14 14:52:11 -0300

    Update Lista

--- frontend/src/components/Lista/Lista.style.tsx
+++ frontend/src/components/Lista/Lista.style.tsx
@@ -1,10 +1,37 @@
+import { styled } from "@mui/material";
 
 
+export const ListaStyled = styled('ul')`
+    
+`;
+
+export const ItemLista = styled('li')`
+    
+`;
+
+export const Foto = styled('img')`
+    
+`;
+
+export const Informacoes = styled('div')`
+    
+`;
+
+export const Nome = styled('h3')`
+    
+`;
 
 
+export const Valor = styled('p')`
+    margin: 0;
+    font-weight: bold;
+    color: ${({theme}) => theme.palette.primary.main };
+`;
 
 
-
+export const Descricao = styled('p')`
+    
+`;
 
 
 
--- frontend/src/components/Lista/Lista.tsx
+++ frontend/src/components/Lista/Lista.tsx
@@ -1,9 +1,19 @@
+import { Button } from "@mui/material";
+import {  ListaStyled, ItemLista, Foto, Informacoes, Nome, Valor, Descricao } from "./Lista.style";
 
 const Lista = () => {
     return (
-        <>
-            <div>Lista Components</div>
-        </>
+            <ListaStyled >
+                <ItemLista>
+                    <Foto src="https://github.com/daniel-oliv3.png"></Foto>
+                    <Informacoes>
+                        <Nome>Daniel Oliveira</Nome>
+                        <Valor>R$ 100,00 por hora</Valor>
+                        <Descricao>Aulas de Skateboarding</Descricao>
+                        <Button>Marcar Aula</Button>
+                    </Informacoes>
+                </ItemLista>
+            </ListaStyled>
     )
 }
 
